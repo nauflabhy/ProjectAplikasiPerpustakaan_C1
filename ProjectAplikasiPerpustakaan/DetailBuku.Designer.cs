3da72edeb0b093a65a0fd0797a359b1f9e55b999
nauflabhy <n4uf4l.ku@gmail.com>
2026-04-14 10:32:52 +0700

    DetailBuku add design

--- ProjectAplikasiPerpustakaan/DetailBuku.Designer.cs
+++ ProjectAplikasiPerpustakaan/DetailBuku.Designer.cs
@@ -28,19 +28,262 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.btnTutup = new System.Windows.Forms.Button();
+            this.lblLokasi = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.lblStokTersedia = new System.Windows.Forms.Label();
+            this.lblJudul = new System.Windows.Forms.Label();
+            this.lblKodeBuku = new System.Windows.Forms.Label();
+            this.lblStokTotal = new System.Windows.Forms.Label();
+            this.lblKategori = new System.Windows.Forms.Label();
+            this.lblPengarang = new System.Windows.Forms.Label();
+            this.lblTahunTerbit = new System.Windows.Forms.Label();
+            this.lblPenerbit = new System.Windows.Forms.Label();
+            this.label14 = new System.Windows.Forms.Label();
+            this.Judul = new System.Windows.Forms.Label();
+            this.KodeBuku = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // btnTutup
+            // 
+            this.btnTutup.Location = new System.Drawing.Point(888, 666);
+            this.btnTutup.Name = "btnTutup";
+            this.btnTutup.Size = new System.Drawing.Size(164, 57);
+            this.btnTutup.TabIndex = 70;
+            this.btnTutup.Text = "Tutup";
+            this.btnTutup.UseVisualStyleBackColor = true;
+            // 
+            // lblLokasi
+            // 
+            this.lblLokasi.AutoSize = true;
+            this.lblLokasi.Location = new System.Drawing.Point(1041, 591);
+            this.lblLokasi.Name = "lblLokasi";
+            this.lblLokasi.Size = new System.Drawing.Size(55, 20);
+            this.lblLokasi.TabIndex = 69;
+            this.lblLokasi.Text = "Lokasi";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(785, 591);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(59, 20);
+            this.label8.TabIndex = 68;
+            this.label8.Text = "Lokasi:";
+            // 
+            // lblStokTersedia
+            // 
+            this.lblStokTersedia.AutoSize = true;
+            this.lblStokTersedia.Location = new System.Drawing.Point(1041, 536);
+            this.lblStokTersedia.Name = "lblStokTersedia";
+            this.lblStokTersedia.Size = new System.Drawing.Size(103, 20);
+            this.lblStokTersedia.TabIndex = 67;
+            this.lblStokTersedia.Text = "StokTersedia";
+            // 
+            // lblJudul
+            // 
+            this.lblJudul.AutoSize = true;
+            this.lblJudul.Location = new System.Drawing.Point(1041, 228);
+            this.lblJudul.Name = "lblJudul";
+            this.lblJudul.Size = new System.Drawing.Size(88, 20);
+            this.lblJudul.TabIndex = 66;
+            this.lblJudul.Text = "JudulBuku:";
+            // 
+            // lblKodeBuku
+            // 
+            this.lblKodeBuku.AutoSize = true;
+            this.lblKodeBuku.Location = new System.Drawing.Point(1041, 183);
+            this.lblKodeBuku.Name = "lblKodeBuku";
+            this.lblKodeBuku.Size = new System.Drawing.Size(87, 20);
+            this.lblKodeBuku.TabIndex = 65;
+            this.lblKodeBuku.Text = "KodeBuku:";
+            // 
+            // lblStokTotal
+            // 
+            this.lblStokTotal.AutoSize = true;
+            this.lblStokTotal.Location = new System.Drawing.Point(1041, 478);
+            this.lblStokTotal.Name = "lblStokTotal";
+            this.lblStokTotal.Size = new System.Drawing.Size(77, 20);
+            this.lblStokTotal.TabIndex = 64;
+            this.lblStokTotal.Text = "StokTotal";
+            // 
+            // lblKategori
+            // 
+            this.lblKategori.AutoSize = true;
+            this.lblKategori.Location = new System.Drawing.Point(1041, 425);
+            this.lblKategori.Name = "lblKategori";
+            this.lblKategori.Size = new System.Drawing.Size(68, 20);
+            this.lblKategori.TabIndex = 63;
+            this.lblKategori.Text = "Kategori";
+            // 
+            // lblPengarang
+            // 
+            this.lblPengarang.AutoSize = true;
+            this.lblPengarang.Location = new System.Drawing.Point(1041, 279);
+            this.lblPengarang.Name = "lblPengarang";
+            this.lblPengarang.Size = new System.Drawing.Size(87, 20);
+            this.lblPengarang.TabIndex = 62;
+            this.lblPengarang.Text = "Pengarang";
+            // 
+            // lblTahunTerbit
+            // 
+            this.lblTahunTerbit.AutoSize = true;
+            this.lblTahunTerbit.Location = new System.Drawing.Point(1041, 372);
+            this.lblTahunTerbit.Name = "lblTahunTerbit";
+            this.lblTahunTerbit.Size = new System.Drawing.Size(94, 20);
+            this.lblTahunTerbit.TabIndex = 61;
+            this.lblTahunTerbit.Text = "TahunTerbit";
+            // 
+            // lblPenerbit
+            // 
+            this.lblPenerbit.AutoSize = true;
+            this.lblPenerbit.Location = new System.Drawing.Point(1041, 326);
+            this.lblPenerbit.Name = "lblPenerbit";
+            this.lblPenerbit.Size = new System.Drawing.Size(68, 20);
+            this.lblPenerbit.TabIndex = 60;
+            this.lblPenerbit.Text = "Penerbit";
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Location = new System.Drawing.Point(785, 536);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(111, 20);
+            this.label14.TabIndex = 59;
+            this.label14.Text = "Stok Tersedia:";
+            // 
+            // Judul
+            // 
+            this.Judul.AutoSize = true;
+            this.Judul.Location = new System.Drawing.Point(785, 228);
+            this.Judul.Name = "Judul";
+            this.Judul.Size = new System.Drawing.Size(88, 20);
+            this.Judul.TabIndex = 58;
+            this.Judul.Text = "JudulBuku:";
+            // 
+            // KodeBuku
+            // 
+            this.KodeBuku.AutoSize = true;
+            this.KodeBuku.Location = new System.Drawing.Point(785, 183);
+            this.KodeBuku.Name = "KodeBuku";
+            this.KodeBuku.Size = new System.Drawing.Size(87, 20);
+            this.KodeBuku.TabIndex = 57;
+            this.KodeBuku.Text = "KodeBuku:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(785, 478);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(85, 20);
+            this.label7.TabIndex = 56;
+            this.label7.Text = "Stok Total:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(785, 425);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(72, 20);
+            this.label5.TabIndex = 55;
+            this.label5.Text = "Kategori:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(785, 279);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(91, 20);
+            this.label4.TabIndex = 54;
+            this.label4.Text = "Pengarang:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(785, 372);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(102, 20);
+            this.label3.TabIndex = 53;
+            this.label3.Text = "Tahun Terbit:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(785, 326);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(72, 20);
+            this.label2.TabIndex = 52;
+            this.label2.Text = "Penerbit:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(881, 85);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(171, 32);
+            this.label1.TabIndex = 51;
+            this.label1.Text = "Detail Buku";
             // 
             // DetailBuku
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1821, 839);
+            this.Controls.Add(this.btnTutup);
+            this.Controls.Add(this.lblLokasi);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.lblStokTersedia);
+            this.Controls.Add(this.lblJudul);
+            this.Controls.Add(this.lblKodeBuku);
+            this.Controls.Add(this.lblStokTotal);
+            this.Controls.Add(this.lblKategori);
+            this.Controls.Add(this.lblPengarang);
+            this.Controls.Add(this.lblTahunTerbit);
+            this.Controls.Add(this.lblPenerbit);
+            this.Controls.Add(this.label14);
+            this.Controls.Add(this.Judul);
+            this.Controls.Add(this.KodeBuku);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Name = "DetailBuku";
             this.Text = "DetailBuku";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnTutup;
+        private System.Windows.Forms.Label lblLokasi;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label lblStokTersedia;
+        private System.Windows.Forms.Label lblJudul;
+        private System.Windows.Forms.Label lblKodeBuku;
+        private System.Windows.Forms.Label lblStokTotal;
+        private System.Windows.Forms.Label lblKategori;
+        private System.Windows.Forms.Label lblPengarang;
+        private System.Windows.Forms.Label lblTahunTerbit;
+        private System.Windows.Forms.Label lblPenerbit;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.Label Judul;
+        private System.Windows.Forms.Label KodeBuku;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
     }
 }
