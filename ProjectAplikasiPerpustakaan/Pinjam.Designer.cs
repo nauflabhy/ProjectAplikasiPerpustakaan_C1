fd5835becf7b06addde678eca0b21cee43fd5edc
nauflabhy <n4uf4l.ku@gmail.com>
2026-04-14 10:35:49 +0700

    Add Design For form Pinjam

--- ProjectAplikasiPerpustakaan/Pinjam.Designer.cs
+++ ProjectAplikasiPerpustakaan/Pinjam.Designer.cs
@@ -28,19 +28,192 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.lblJudul = new System.Windows.Forms.Label();
+            this.lblKodeBuku = new System.Windows.Forms.Label();
+            this.btnKembali = new System.Windows.Forms.Button();
+            this.btnAjukanPeminjaman = new System.Windows.Forms.Button();
+            this.txtPerguruan = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.txtNoHp = new System.Windows.Forms.TextBox();
+            this.txtNamaLengkap = new System.Windows.Forms.TextBox();
+            this.txtNIK = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // lblJudul
+            // 
+            this.lblJudul.AutoSize = true;
+            this.lblJudul.Location = new System.Drawing.Point(1092, 185);
+            this.lblJudul.Name = "lblJudul";
+            this.lblJudul.Size = new System.Drawing.Size(84, 20);
+            this.lblJudul.TabIndex = 36;
+            this.lblJudul.Text = "JudulBuku";
+            // 
+            // lblKodeBuku
+            // 
+            this.lblKodeBuku.AutoSize = true;
+            this.lblKodeBuku.Location = new System.Drawing.Point(878, 185);
+            this.lblKodeBuku.Name = "lblKodeBuku";
+            this.lblKodeBuku.Size = new System.Drawing.Size(83, 20);
+            this.lblKodeBuku.TabIndex = 35;
+            this.lblKodeBuku.Text = "KodeBuku";
+            // 
+            // btnKembali
+            // 
+            this.btnKembali.Location = new System.Drawing.Point(839, 537);
+            this.btnKembali.Name = "btnKembali";
+            this.btnKembali.Size = new System.Drawing.Size(165, 49);
+            this.btnKembali.TabIndex = 34;
+            this.btnKembali.Text = "Kembali";
+            this.btnKembali.UseVisualStyleBackColor = true;
+            // 
+            // btnAjukanPeminjaman
+            // 
+            this.btnAjukanPeminjaman.Location = new System.Drawing.Point(1086, 537);
+            this.btnAjukanPeminjaman.Name = "btnAjukanPeminjaman";
+            this.btnAjukanPeminjaman.Size = new System.Drawing.Size(169, 49);
+            this.btnAjukanPeminjaman.TabIndex = 33;
+            this.btnAjukanPeminjaman.Text = "Ajukan Peminjaman";
+            this.btnAjukanPeminjaman.UseVisualStyleBackColor = true;
+            // 
+            // txtPerguruan
+            // 
+            this.txtPerguruan.Location = new System.Drawing.Point(1005, 458);
+            this.txtPerguruan.Name = "txtPerguruan";
+            this.txtPerguruan.Size = new System.Drawing.Size(231, 26);
+            this.txtPerguruan.TabIndex = 32;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(838, 461);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(145, 20);
+            this.label7.TabIndex = 31;
+            this.label7.Text = "Perguruan/Sekolah";
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(1005, 408);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(231, 26);
+            this.txtEmail.TabIndex = 30;
+            // 
+            // txtNoHp
+            // 
+            this.txtNoHp.Location = new System.Drawing.Point(1005, 359);
+            this.txtNoHp.Name = "txtNoHp";
+            this.txtNoHp.Size = new System.Drawing.Size(231, 26);
+            this.txtNoHp.TabIndex = 29;
+            // 
+            // txtNamaLengkap
+            // 
+            this.txtNamaLengkap.Location = new System.Drawing.Point(1005, 309);
+            this.txtNamaLengkap.Name = "txtNamaLengkap";
+            this.txtNamaLengkap.Size = new System.Drawing.Size(231, 26);
+            this.txtNamaLengkap.TabIndex = 28;
+            // 
+            // txtNIK
+            // 
+            this.txtNIK.Location = new System.Drawing.Point(1005, 262);
+            this.txtNIK.Name = "txtNIK";
+            this.txtNIK.Size = new System.Drawing.Size(231, 26);
+            this.txtNIK.TabIndex = 27;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(838, 408);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(48, 20);
+            this.label5.TabIndex = 26;
+            this.label5.Text = "Email";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(838, 265);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 20);
+            this.label4.TabIndex = 25;
+            this.label4.Text = "NIK";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(838, 359);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(54, 20);
+            this.label3.TabIndex = 24;
+            this.label3.Text = "No Hp";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(838, 309);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(117, 20);
+            this.label1.TabIndex = 23;
+            this.label1.Text = "Nama Lengkap";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Roboto Mono", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(882, 111);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(294, 32);
+            this.label2.TabIndex = 22;
+            this.label2.Text = "FORM PEMINJAMAN BUKU";
             // 
             // Pinjam
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1822, 830);
+            this.Controls.Add(this.lblJudul);
+            this.Controls.Add(this.lblKodeBuku);
+            this.Controls.Add(this.btnKembali);
+            this.Controls.Add(this.btnAjukanPeminjaman);
+            this.Controls.Add(this.txtPerguruan);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.txtNoHp);
+            this.Controls.Add(this.txtNamaLengkap);
+            this.Controls.Add(this.txtNIK);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label2);
             this.Name = "Pinjam";
             this.Text = "Pinjam";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblJudul;
+        private System.Windows.Forms.Label lblKodeBuku;
+        private System.Windows.Forms.Button btnKembali;
+        private System.Windows.Forms.Button btnAjukanPeminjaman;
+        private System.Windows.Forms.TextBox txtPerguruan;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.TextBox txtNoHp;
+        private System.Windows.Forms.TextBox txtNamaLengkap;
+        private System.Windows.Forms.TextBox txtNIK;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
