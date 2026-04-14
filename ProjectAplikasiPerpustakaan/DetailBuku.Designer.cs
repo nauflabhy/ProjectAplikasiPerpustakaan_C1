eeade67d3d6a870a2c62c4dd9c52fd63a27c299e
nauflabhy <n4uf4l.ku@gmail.com>
2026-04-14 10:44:52 +0700

    Fix DetailBuku form

--- ProjectAplikasiPerpustakaan/DetailBuku.Designer.cs
+++ ProjectAplikasiPerpustakaan/DetailBuku.Designer.cs
@@ -52,7 +52,7 @@
             // 
             // btnTutup
             // 
-            this.btnTutup.Location = new System.Drawing.Point(888, 666);
+            this.btnTutup.Location = new System.Drawing.Point(837, 691);
             this.btnTutup.Name = "btnTutup";
             this.btnTutup.Size = new System.Drawing.Size(164, 57);
             this.btnTutup.TabIndex = 70;
@@ -62,17 +62,16 @@
             // lblLokasi
             // 
             this.lblLokasi.AutoSize = true;
-            this.lblLokasi.Location = new System.Drawing.Point(1041, 591);
+            this.lblLokasi.Location = new System.Drawing.Point(990, 616);
             this.lblLokasi.Name = "lblLokasi";
             this.lblLokasi.Size = new System.Drawing.Size(55, 20);
             this.lblLokasi.TabIndex = 69;
             this.lblLokasi.Text = "Lokasi";
-            this.lblLokasi.Click += new System.EventHandler(this.lblLokasi_Click);
             // 
             // label8
             // 
             this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(785, 591);
+            this.label8.Location = new System.Drawing.Point(734, 616);
             this.label8.Name = "label8";
             this.label8.Size = new System.Drawing.Size(59, 20);
             this.label8.TabIndex = 68;
@@ -81,87 +80,79 @@
             // lblStokTersedia
             // 
             this.lblStokTersedia.AutoSize = true;
-            this.lblStokTersedia.Location = new System.Drawing.Point(1041, 536);
+            this.lblStokTersedia.Location = new System.Drawing.Point(990, 561);
             this.lblStokTersedia.Name = "lblStokTersedia";
             this.lblStokTersedia.Size = new System.Drawing.Size(103, 20);
             this.lblStokTersedia.TabIndex = 67;
             this.lblStokTersedia.Text = "StokTersedia";
-            this.lblStokTersedia.Click += new System.EventHandler(this.lblStokTersedia_Click);
             // 
             // lblJudul
             // 
             this.lblJudul.AutoSize = true;
-            this.lblJudul.Location = new System.Drawing.Point(1041, 228);
+            this.lblJudul.Location = new System.Drawing.Point(990, 253);
             this.lblJudul.Name = "lblJudul";
             this.lblJudul.Size = new System.Drawing.Size(88, 20);
             this.lblJudul.TabIndex = 66;
             this.lblJudul.Text = "JudulBuku:";
-            this.lblJudul.Click += new System.EventHandler(this.lblJudul_Click);
             // 
             // lblKodeBuku
             // 
             this.lblKodeBuku.AutoSize = true;
-            this.lblKodeBuku.Location = new System.Drawing.Point(1041, 183);
+            this.lblKodeBuku.Location = new System.Drawing.Point(990, 208);
             this.lblKodeBuku.Name = "lblKodeBuku";
             this.lblKodeBuku.Size = new System.Drawing.Size(87, 20);
             this.lblKodeBuku.TabIndex = 65;
             this.lblKodeBuku.Text = "KodeBuku:";
-            this.lblKodeBuku.Click += new System.EventHandler(this.lblKodeBuku_Click);
             // 
             // lblStokTotal
             // 
             this.lblStokTotal.AutoSize = true;
-            this.lblStokTotal.Location = new System.Drawing.Point(1041, 478);
+            this.lblStokTotal.Location = new System.Drawing.Point(990, 503);
             this.lblStokTotal.Name = "lblStokTotal";
             this.lblStokTotal.Size = new System.Drawing.Size(77, 20);
             this.lblStokTotal.TabIndex = 64;
             this.lblStokTotal.Text = "StokTotal";
-            this.lblStokTotal.Click += new System.EventHandler(this.lblStokTotal_Click);
             // 
             // lblKategori
             // 
             this.lblKategori.AutoSize = true;
-            this.lblKategori.Location = new System.Drawing.Point(1041, 425);
+            this.lblKategori.Location = new System.Drawing.Point(990, 450);
             this.lblKategori.Name = "lblKategori";
             this.lblKategori.Size = new System.Drawing.Size(68, 20);
             this.lblKategori.TabIndex = 63;
             this.lblKategori.Text = "Kategori";
-            this.lblKategori.Click += new System.EventHandler(this.lblKategori_Click);
             // 
             // lblPengarang
             // 
             this.lblPengarang.AutoSize = true;
-            this.lblPengarang.Location = new System.Drawing.Point(1041, 279);
+            this.lblPengarang.Location = new System.Drawing.Point(990, 304);
             this.lblPengarang.Name = "lblPengarang";
             this.lblPengarang.Size = new System.Drawing.Size(87, 20);
             this.lblPengarang.TabIndex = 62;
             this.lblPengarang.Text = "Pengarang";
-            this.lblPengarang.Click += new System.EventHandler(this.lblPengarang_Click);
             // 
             // lblTahunTerbit
             // 
             this.lblTahunTerbit.AutoSize = true;
-            this.lblTahunTerbit.Location = new System.Drawing.Point(1041, 372);
+            this.lblTahunTerbit.Location = new System.Drawing.Point(990, 397);
             this.lblTahunTerbit.Name = "lblTahunTerbit";
             this.lblTahunTerbit.Size = new System.Drawing.Size(94, 20);
             this.lblTahunTerbit.TabIndex = 61;
             this.lblTahunTerbit.Text = "TahunTerbit";
-            this.lblTahunTerbit.Click += new System.EventHandler(this.lblTahunTerbit_Click);
             // 
             // lblPenerbit
             // 
             this.lblPenerbit.AutoSize = true;
-            this.lblPenerbit.Location = new System.Drawing.Point(1041, 326);
+            this.lblPenerbit.Location = new System.Drawing.Point(990, 351);
             this.lblPenerbit.Name = "lblPenerbit";
             this.lblPenerbit.Size = new System.Drawing.Size(68, 20);
             this.lblPenerbit.TabIndex = 60;
             this.lblPenerbit.Text = "Penerbit";
-            this.lblPenerbit.Click += new System.EventHandler(this.lblPenerbit_Click);
             // 
             // label14
             // 
             this.label14.AutoSize = true;
-            this.label14.Location = new System.Drawing.Point(785, 536);
+            this.label14.Location = new System.Drawing.Point(734, 561);
             this.label14.Name = "label14";
             this.label14.Size = new System.Drawing.Size(111, 20);
             this.label14.TabIndex = 59;
@@ -170,7 +161,7 @@
             // Judul
             // 
             this.Judul.AutoSize = true;
-            this.Judul.Location = new System.Drawing.Point(785, 228);
+            this.Judul.Location = new System.Drawing.Point(734, 253);
             this.Judul.Name = "Judul";
             this.Judul.Size = new System.Drawing.Size(88, 20);
             this.Judul.TabIndex = 58;
@@ -179,7 +170,7 @@
             // KodeBuku
             // 
             this.KodeBuku.AutoSize = true;
-            this.KodeBuku.Location = new System.Drawing.Point(785, 183);
+            this.KodeBuku.Location = new System.Drawing.Point(734, 208);
             this.KodeBuku.Name = "KodeBuku";
             this.KodeBuku.Size = new System.Drawing.Size(87, 20);
             this.KodeBuku.TabIndex = 57;
@@ -188,7 +179,7 @@
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(785, 478);
+            this.label7.Location = new System.Drawing.Point(734, 503);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(85, 20);
             this.label7.TabIndex = 56;
@@ -197,7 +188,7 @@
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(785, 425);
+            this.label5.Location = new System.Drawing.Point(734, 450);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(72, 20);
             this.label5.TabIndex = 55;
@@ -206,7 +197,7 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(785, 279);
+            this.label4.Location = new System.Drawing.Point(734, 304);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(91, 20);
             this.label4.TabIndex = 54;
@@ -215,7 +206,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(785, 372);
+            this.label3.Location = new System.Drawing.Point(734, 397);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(102, 20);
             this.label3.TabIndex = 53;
@@ -224,7 +215,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(785, 326);
+            this.label2.Location = new System.Drawing.Point(734, 351);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(72, 20);
             this.label2.TabIndex = 52;
@@ -234,7 +225,7 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(881, 85);
+            this.label1.Location = new System.Drawing.Point(830, 110);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(171, 32);
             this.label1.TabIndex = 51;
@@ -244,7 +235,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1821, 839);
+            this.ClientSize = new System.Drawing.Size(1827, 859);
             this.Controls.Add(this.btnTutup);
             this.Controls.Add(this.lblLokasi);
             this.Controls.Add(this.label8);
