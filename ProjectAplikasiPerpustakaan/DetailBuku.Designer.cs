df129ab0ce587401559ecd4c13e29786d854fed2
nauflabhy <n4uf4l.ku@gmail.com>
2026-04-14 10:32:31 +0700

    DetailBuku Add Form

---
+++ ProjectAplikasiPerpustakaan/DetailBuku.Designer.cs
@@ -0,0 +1,46 @@
+﻿namespace ProjectAplikasiPerpustakaan
+{
+    partial class DetailBuku
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // DetailBuku
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1821, 839);
+            this.Name = "DetailBuku";
+            this.Text = "DetailBuku";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
