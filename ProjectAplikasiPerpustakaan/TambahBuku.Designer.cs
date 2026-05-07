3ec2f002a4eea4781f1d934596f37f3328dcd579
nauflabhy <n4uf4l.ku@gmail.com>
2026-05-07 20:18:32 +0700

    add tambahbuku form

---
+++ ProjectAplikasiPerpustakaan/TambahBuku.Designer.cs
@@ -0,0 +1,282 @@
+﻿namespace ProjectAplikasiPerpustakaan
+{
+    partial class TambahBuku
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btlBatal = new System.Windows.Forms.Button();
+            this.btnTambah = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.txtKodeBuku = new System.Windows.Forms.TextBox();
+            this.txtJudul = new System.Windows.Forms.TextBox();
+            this.txtPengarang = new System.Windows.Forms.TextBox();
+            this.txtPenerbit = new System.Windows.Forms.TextBox();
+            this.txtTahunTerbit = new System.Windows.Forms.TextBox();
+            this.txtKategori = new System.Windows.Forms.TextBox();
+            this.txtStokTotal = new System.Windows.Forms.TextBox();
+            this.txtStokTersedia = new System.Windows.Forms.TextBox();
+            this.txtLokasi = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btlBatal
+            // 
+            this.btlBatal.Location = new System.Drawing.Point(634, 660);
+            this.btlBatal.Name = "btlBatal";
+            this.btlBatal.Size = new System.Drawing.Size(208, 72);
+            this.btlBatal.TabIndex = 0;
+            this.btlBatal.Text = "Batal";
+            this.btlBatal.UseVisualStyleBackColor = true;
+            this.btlBatal.Click += new System.EventHandler(this.btlBatal_Click);
+            // 
+            // btnTambah
+            // 
+            this.btnTambah.Location = new System.Drawing.Point(896, 660);
+            this.btnTambah.Name = "btnTambah";
+            this.btnTambah.Size = new System.Drawing.Size(208, 72);
+            this.btnTambah.TabIndex = 1;
+            this.btnTambah.Text = "Tambah";
+            this.btnTambah.UseVisualStyleBackColor = true;
+            this.btnTambah.Click += new System.EventHandler(this.btnTambah_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(633, 90);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(91, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Kode Buku:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(633, 150);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(51, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Judul:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(633, 203);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(91, 20);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Pengarang:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(633, 261);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(72, 20);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Penerbit:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(633, 314);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(102, 20);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "Tahun Terbit:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(633, 368);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(72, 20);
+            this.label7.TabIndex = 8;
+            this.label7.Text = "Kategori:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(633, 425);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(85, 20);
+            this.label8.TabIndex = 9;
+            this.label8.Text = "Stok Total:";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(633, 487);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(111, 20);
+            this.label9.TabIndex = 10;
+            this.label9.Text = "Stok Tersedia:";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(633, 555);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(59, 20);
+            this.label10.TabIndex = 11;
+            this.label10.Text = "Lokasi:";
+            // 
+            // txtKodeBuku
+            // 
+            this.txtKodeBuku.Location = new System.Drawing.Point(839, 90);
+            this.txtKodeBuku.Name = "txtKodeBuku";
+            this.txtKodeBuku.Size = new System.Drawing.Size(265, 26);
+            this.txtKodeBuku.TabIndex = 12;
+            this.txtKodeBuku.TextChanged += new System.EventHandler(this.txtKodeBuku_TextChanged);
+            // 
+            // txtJudul
+            // 
+            this.txtJudul.Location = new System.Drawing.Point(839, 150);
+            this.txtJudul.Name = "txtJudul";
+            this.txtJudul.Size = new System.Drawing.Size(265, 26);
+            this.txtJudul.TabIndex = 13;
+            this.txtJudul.TextChanged += new System.EventHandler(this.txtJudul_TextChanged);
+            // 
+            // txtPengarang
+            // 
+            this.txtPengarang.Location = new System.Drawing.Point(839, 203);
+            this.txtPengarang.Name = "txtPengarang";
+            this.txtPengarang.Size = new System.Drawing.Size(265, 26);
+            this.txtPengarang.TabIndex = 14;
+            this.txtPengarang.TextChanged += new System.EventHandler(this.txtPengarang_TextChanged);
+            // 
+            // txtPenerbit
+            // 
+            this.txtPenerbit.Location = new System.Drawing.Point(839, 255);
+            this.txtPenerbit.Name = "txtPenerbit";
+            this.txtPenerbit.Size = new System.Drawing.Size(265, 26);
+            this.txtPenerbit.TabIndex = 15;
+            this.txtPenerbit.TextChanged += new System.EventHandler(this.txtPenerbit_TextChanged);
+            // 
+            // txtTahunTerbit
+            // 
+            this.txtTahunTerbit.Location = new System.Drawing.Point(839, 308);
+            this.txtTahunTerbit.Name = "txtTahunTerbit";
+            this.txtTahunTerbit.Size = new System.Drawing.Size(265, 26);
+            this.txtTahunTerbit.TabIndex = 16;
+            this.txtTahunTerbit.TextChanged += new System.EventHandler(this.txtTahunTerbit_TextChanged);
+            // 
+            // txtKategori
+            // 
+            this.txtKategori.Location = new System.Drawing.Point(839, 368);
+            this.txtKategori.Name = "txtKategori";
+            this.txtKategori.Size = new System.Drawing.Size(265, 26);
+            this.txtKategori.TabIndex = 17;
+            this.txtKategori.TextChanged += new System.EventHandler(this.txtKategori_TextChanged);
+            // 
+            // txtStokTotal
+            // 
+            this.txtStokTotal.Location = new System.Drawing.Point(839, 425);
+            this.txtStokTotal.Name = "txtStokTotal";
+            this.txtStokTotal.Size = new System.Drawing.Size(265, 26);
+            this.txtStokTotal.TabIndex = 18;
+            this.txtStokTotal.TextChanged += new System.EventHandler(this.txtStokTotal_TextChanged);
+            // 
+            // txtStokTersedia
+            // 
+            this.txtStokTersedia.Location = new System.Drawing.Point(839, 487);
+            this.txtStokTersedia.Name = "txtStokTersedia";
+            this.txtStokTersedia.Size = new System.Drawing.Size(265, 26);
+            this.txtStokTersedia.TabIndex = 19;
+            this.txtStokTersedia.TextChanged += new System.EventHandler(this.txtStokTersedia_TextChanged);
+            // 
+            // txtLokasi
+            // 
+            this.txtLokasi.Location = new System.Drawing.Point(839, 552);
+            this.txtLokasi.Name = "txtLokasi";
+            this.txtLokasi.Size = new System.Drawing.Size(265, 26);
+            this.txtLokasi.TabIndex = 20;
+            this.txtLokasi.TextChanged += new System.EventHandler(this.txtLokasi_TextChanged);
+            // 
+            // TambahBuku
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1691, 844);
+            this.Controls.Add(this.txtLokasi);
+            this.Controls.Add(this.txtStokTersedia);
+            this.Controls.Add(this.txtStokTotal);
+            this.Controls.Add(this.txtKategori);
+            this.Controls.Add(this.txtTahunTerbit);
+            this.Controls.Add(this.txtPenerbit);
+            this.Controls.Add(this.txtPengarang);
+            this.Controls.Add(this.txtJudul);
+            this.Controls.Add(this.txtKodeBuku);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnTambah);
+            this.Controls.Add(this.btlBatal);
+            this.Name = "TambahBuku";
+            this.Text = "TambahBuku";
+            this.Load += new System.EventHandler(this.TambahBuku_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btlBatal;
+        private System.Windows.Forms.Button btnTambah;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox txtKodeBuku;
+        private System.Windows.Forms.TextBox txtJudul;
+        private System.Windows.Forms.TextBox txtPengarang;
+        private System.Windows.Forms.TextBox txtPenerbit;
+        private System.Windows.Forms.TextBox txtTahunTerbit;
+        private System.Windows.Forms.TextBox txtKategori;
+        private System.Windows.Forms.TextBox txtStokTotal;
+        private System.Windows.Forms.TextBox txtStokTersedia;
+        private System.Windows.Forms.TextBox txtLokasi;
+    }
+}
---
+++ ProjectAplikasiPerpustakaan/TambahBuku.cs
@@ -0,0 +1,230 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace ProjectAplikasiPerpustakaan
+{
+    public partial class TambahBuku : Form
+    {
+        private readonly string connectionString =
+            "Data Source=NAUFAL\\NZO2;Initial Catalog=db_perpustakaan;Integrated Security=True";
+
+        public TambahBuku()
+        {
+            InitializeComponent();
+        }
+
+        private void TambahBuku_Load(object sender, EventArgs e)
+        {
+            // Setup pembatasan input
+            SetupInputRestrictions();
+
+            // Optional: Set fokus ke field pertama
+            txtKodeBuku.Focus();
+        }
+
+        private void SetupInputRestrictions()
+        {
+            // Kode Buku → Huruf + Angka + "-" (contoh: B001, BK-2025)
+            txtKodeBuku.KeyPress += (s, ev) =>
+            {
+                if (!char.IsLetterOrDigit(ev.KeyChar) && ev.KeyChar != '-' && ev.KeyChar != '\b')
+                    ev.Handled = true;
+            };
+            txtKodeBuku.MaxLength = 20;
+
+            // Judul, Pengarang, Penerbit, Kategori, Lokasi → Huruf, Angka, Spasi
+            txtJudul.KeyPress += AllowAlphanumericWithSpace;
+            txtPengarang.KeyPress += AllowAlphanumericWithSpace;
+            txtPenerbit.KeyPress += AllowAlphanumericWithSpace;
+            txtKategori.KeyPress += AllowAlphanumericWithSpace;
+            txtLokasi.KeyPress += AllowAlphanumericWithSpace;
+
+            txtJudul.MaxLength = 200;
+            txtPengarang.MaxLength = 100;
+            txtPenerbit.MaxLength = 100;
+            txtKategori.MaxLength = 50;
+            txtLokasi.MaxLength = 50;
+
+            // Tahun Terbit → Hanya Angka
+            txtTahunTerbit.KeyPress += AllowOnlyNumbers;
+            txtTahunTerbit.MaxLength = 4;
+
+            // Stok Total & Stok Tersedia → Hanya Angka
+            txtStokTotal.KeyPress += AllowOnlyNumbers;
+            txtStokTersedia.KeyPress += AllowOnlyNumbers;
+            txtStokTotal.MaxLength = 5;
+            txtStokTersedia.MaxLength = 5;
+        }
+
+        private void AllowAlphanumericWithSpace(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsLetterOrDigit(e.KeyChar) &&
+                !char.IsWhiteSpace(e.KeyChar) &&
+                e.KeyChar != '\b')
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void AllowOnlyNumbers(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsDigit(e.KeyChar) && e.KeyChar != '\b')
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void txtKodeBuku_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtJudul_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtPengarang_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtPenerbit_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtTahunTerbit_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtKategori_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtStokTotal_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtStokTersedia_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtLokasi_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btlBatal_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Batalkan penambahan buku?\nData yang sudah diisi akan hilang.",
+                "Konfirmasi", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+            {
+                this.Close();
+            }
+        }
+
+        private void btnTambah_Click(object sender, EventArgs e)
+        {
+            // Validasi input wajib
+            if (string.IsNullOrWhiteSpace(txtKodeBuku.Text) ||
+                string.IsNullOrWhiteSpace(txtJudul.Text) ||
+                string.IsNullOrWhiteSpace(txtPengarang.Text) ||
+                string.IsNullOrWhiteSpace(txtStokTotal.Text) ||
+                string.IsNullOrWhiteSpace(txtStokTersedia.Text))
+            {
+                MessageBox.Show("Kode Buku, Judul, Pengarang, Stok Total, dan Stok Tersedia wajib diisi!",
+                    "Peringatan", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            // Validasi stok
+            if (!int.TryParse(txtStokTotal.Text, out int stokTotal) ||
+                !int.TryParse(txtStokTersedia.Text, out int stokTersedia))
+            {
+                MessageBox.Show("Stok harus berupa angka yang valid!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            if (stokTersedia > stokTotal)
+            {
+                MessageBox.Show("Stok Tersedia tidak boleh lebih besar dari Stok Total!",
+                    "Validasi Stok", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    conn.Open();
+
+                    string query = @"
+                        INSERT INTO BUKU 
+                        (kode_buku, judul, pengarang, penerbit, tahun_terbit, kategori, 
+                         stok_total, stok_tersedia, lokasi)
+                        VALUES 
+                        (@kode_buku, @judul, @pengarang, @penerbit, @tahun_terbit, @kategori, 
+                         @stok_total, @stok_tersedia, @lokasi)";
+
+                    using (SqlCommand cmd = new SqlCommand(query, conn))
+                    {
+                        cmd.Parameters.AddWithValue("@kode_buku", txtKodeBuku.Text.Trim().ToUpper());
+                        cmd.Parameters.AddWithValue("@judul", txtJudul.Text.Trim());
+                        cmd.Parameters.AddWithValue("@pengarang", txtPengarang.Text.Trim());
+                        cmd.Parameters.AddWithValue("@penerbit",
+                            string.IsNullOrWhiteSpace(txtPenerbit.Text) ? (object)DBNull.Value : txtPenerbit.Text.Trim());
+                        cmd.Parameters.AddWithValue("@tahun_terbit",
+                            string.IsNullOrWhiteSpace(txtTahunTerbit.Text) ? (object)DBNull.Value : txtTahunTerbit.Text.Trim());
+                        cmd.Parameters.AddWithValue("@kategori",
+                            string.IsNullOrWhiteSpace(txtKategori.Text) ? (object)DBNull.Value : txtKategori.Text.Trim());
+                        cmd.Parameters.AddWithValue("@stok_total", stokTotal);
+                        cmd.Parameters.AddWithValue("@stok_tersedia", stokTersedia);
+                        cmd.Parameters.AddWithValue("@lokasi",
+                            string.IsNullOrWhiteSpace(txtLokasi.Text) ? (object)DBNull.Value : txtLokasi.Text.Trim());
+
+                        int result = cmd.ExecuteNonQuery();
+
+                        if (result > 0)
+                        {
+                            MessageBox.Show("Buku berhasil ditambahkan ke database!",
+                                "Sukses", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                            // Reset form setelah berhasil
+                            ClearForm();
+                            txtKodeBuku.Focus();
+                        }
+                    }
+                }
+            }
+            catch (SqlException ex) when (ex.Number == 2627 || ex.Message.Contains("duplicate"))
+            {
+                MessageBox.Show("Kode Buku sudah ada di database!\nSilakan gunakan kode lain.",
+                    "Duplikat Kode Buku", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Terjadi kesalahan saat menambahkan buku:\n" + ex.Message,
+                    "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void ClearForm()
+        {
+            txtKodeBuku.Clear();
+            txtJudul.Clear();
+            txtPengarang.Clear();
+            txtPenerbit.Clear();
+            txtTahunTerbit.Clear();
+            txtKategori.Clear();
+            txtStokTotal.Clear();
+            txtStokTersedia.Clear();
+            txtLokasi.Clear();
+        }
+    }
+}
