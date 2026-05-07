d3cf7b851f5987bc7a94488e857410b4546a06c9
nauflabhy <n4uf4l.ku@gmail.com>
2026-05-07 20:52:53 +0700

    Menambah form untuk TotalLaporan

---
+++ ProjectAplikasiPerpustakaan/TotalLaporan.Designer.cs
@@ -0,0 +1,182 @@
+﻿namespace ProjectAplikasiPerpustakaan
+{
+    partial class TotalLaporan
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.lblDenda = new System.Windows.Forms.Label();
+            this.lblPengembalian = new System.Windows.Forms.Label();
+            this.lblPeminjaman = new System.Windows.Forms.Label();
+            this.lblTotalKunjungan = new System.Windows.Forms.Label();
+            this.lblPeriode = new System.Windows.Forms.Label();
+            this.btnKembali = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(755, 166);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Periode:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(755, 223);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(128, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Total Kunjungan:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(755, 287);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(139, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Total Peminjaman:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(755, 352);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(153, 20);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Total Pengembalian:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(755, 421);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(100, 20);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Total Denda:";
+            // 
+            // lblDenda
+            // 
+            this.lblDenda.AutoSize = true;
+            this.lblDenda.Location = new System.Drawing.Point(1118, 421);
+            this.lblDenda.Name = "lblDenda";
+            this.lblDenda.Size = new System.Drawing.Size(51, 20);
+            this.lblDenda.TabIndex = 9;
+            this.lblDenda.Text = "label6";
+            // 
+            // lblPengembalian
+            // 
+            this.lblPengembalian.AutoSize = true;
+            this.lblPengembalian.Location = new System.Drawing.Point(1118, 352);
+            this.lblPengembalian.Name = "lblPengembalian";
+            this.lblPengembalian.Size = new System.Drawing.Size(51, 20);
+            this.lblPengembalian.TabIndex = 8;
+            this.lblPengembalian.Text = "label7";
+            // 
+            // lblPeminjaman
+            // 
+            this.lblPeminjaman.AutoSize = true;
+            this.lblPeminjaman.Location = new System.Drawing.Point(1118, 287);
+            this.lblPeminjaman.Name = "lblPeminjaman";
+            this.lblPeminjaman.Size = new System.Drawing.Size(51, 20);
+            this.lblPeminjaman.TabIndex = 7;
+            this.lblPeminjaman.Text = "label8";
+            // 
+            // lblTotalKunjungan
+            // 
+            this.lblTotalKunjungan.AutoSize = true;
+            this.lblTotalKunjungan.Location = new System.Drawing.Point(1118, 223);
+            this.lblTotalKunjungan.Name = "lblTotalKunjungan";
+            this.lblTotalKunjungan.Size = new System.Drawing.Size(51, 20);
+            this.lblTotalKunjungan.TabIndex = 6;
+            this.lblTotalKunjungan.Text = "label9";
+            // 
+            // lblPeriode
+            // 
+            this.lblPeriode.AutoSize = true;
+            this.lblPeriode.Location = new System.Drawing.Point(1118, 166);
+            this.lblPeriode.Name = "lblPeriode";
+            this.lblPeriode.Size = new System.Drawing.Size(60, 20);
+            this.lblPeriode.TabIndex = 5;
+            this.lblPeriode.Text = "label10";
+            // 
+            // btnKembali
+            // 
+            this.btnKembali.Location = new System.Drawing.Point(879, 516);
+            this.btnKembali.Name = "btnKembali";
+            this.btnKembali.Size = new System.Drawing.Size(198, 88);
+            this.btnKembali.TabIndex = 10;
+            this.btnKembali.Text = "Kembali";
+            this.btnKembali.UseVisualStyleBackColor = true;
+            this.btnKembali.Click += new System.EventHandler(this.btnKembali_Click);
+            // 
+            // TotalLaporan
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1799, 841);
+            this.Controls.Add(this.btnKembali);
+            this.Controls.Add(this.lblDenda);
+            this.Controls.Add(this.lblPengembalian);
+            this.Controls.Add(this.lblPeminjaman);
+            this.Controls.Add(this.lblTotalKunjungan);
+            this.Controls.Add(this.lblPeriode);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "TotalLaporan";
+            this.Text = "Laporan";
+            this.Load += new System.EventHandler(this.Laporan_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label lblDenda;
+        private System.Windows.Forms.Label lblPengembalian;
+        private System.Windows.Forms.Label lblPeminjaman;
+        private System.Windows.Forms.Label lblTotalKunjungan;
+        private System.Windows.Forms.Label lblPeriode;
+        private System.Windows.Forms.Button btnKembali;
+    }
+}
---
+++ ProjectAplikasiPerpustakaan/TotalLaporan.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace ProjectAplikasiPerpustakaan
+{
+    public partial class TotalLaporan : Form
+    {
+        private readonly string connectionString =
+            "Data Source=NAUFAL\\NZO2;Initial Catalog=db_perpustakaan;Integrated Security=True";
+
+        public TotalLaporan()
+        {
+            InitializeComponent();
+        }
+
+        private void Laporan_Load(object sender, EventArgs e)
+        {
+            LoadTotalLaporan();
+        }
+
+        // ================== LOAD TOTAL LAPORAN ==================
+        private void LoadTotalLaporan()
+        {
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    conn.Open();
+
+                    string query = @"
+                        SELECT 
+                            FORMAT(GETDATE(), 'MMMM yyyy') AS periode,
+                            
+                            -- Total Kunjungan dihitung dari PEMINJAMAN yang statusnya SELESAI
+                            (SELECT COUNT(*) FROM PEMINJAMAN 
+                             WHERE status = 'selesai') AS total_kunjungan,
+                            
+                            -- Total Peminjaman keseluruhan
+                            (SELECT COUNT(*) FROM PEMINJAMAN) AS total_peminjaman,
+                            
+                            -- Total Pengembalian
+                            (SELECT COUNT(*) FROM PENGEMBALIAN) AS total_pengembalian,
+                            
+                            -- Total Denda
+                            (SELECT ISNULL(SUM(denda), 0) FROM PENGEMBALIAN) AS total_denda;";
+
+                    using (SqlCommand cmd = new SqlCommand(query, conn))
+                    {
+                        using (SqlDataReader reader = cmd.ExecuteReader())
+                        {
+                            if (reader.Read())
+                            {
+                                lblPeriode.Text = reader["periode"].ToString();
+                                lblTotalKunjungan.Text = Convert.ToInt32(reader["total_kunjungan"]).ToString("N0") + " orang";
+                                lblPeminjaman.Text = Convert.ToInt32(reader["total_peminjaman"]).ToString("N0") + " buku";
+                                lblPengembalian.Text = Convert.ToInt32(reader["total_pengembalian"]).ToString("N0") + " buku";
+                                lblDenda.Text = "Rp " + Convert.ToDecimal(reader["total_denda"]).ToString("N0");
+                            }
+                            else
+                            {
+                                ResetLabels();
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Terjadi kesalahan saat memuat data:\n" + ex.Message,
+                    "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                ResetLabels();
+            }
+        }
+
+        private void ResetLabels()
+        {
+            lblPeriode.Text = "-";
+            lblTotalKunjungan.Text = "0 orang";
+            lblPeminjaman.Text = "0 buku";
+            lblPengembalian.Text = "0 buku";
+            lblDenda.Text = "Rp 0";
+        }
+
+
+        private void btnKembali_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
