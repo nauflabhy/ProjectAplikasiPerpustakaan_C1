33597a1e279233f95192b6c08785f02c15d00c56
nauflabhy <n4uf4l.ku@gmail.com>
2026-05-13 18:14:46 +0700

    Menambahkan View untuk LoadDataLaporan di form TotalLaporan

--- ProjectAplikasiPerpustakaan/TotalLaporan.cs
+++ ProjectAplikasiPerpustakaan/TotalLaporan.cs
@@ -30,47 +30,36 @@ namespace ProjectAplikasiPerpustakaan
                 {
                     conn.Open();
 
-                    string query = @"
-                        SELECT 
-                            FORMAT(GETDATE(), 'MMMM yyyy') AS periode,
-                            
-                            -- Total Kunjungan dihitung dari PEMINJAMAN yang statusnya SELESAI
-                            (SELECT COUNT(*) FROM PEMINJAMAN 
-                             WHERE status = 'selesai') AS total_kunjungan,
-                            
-                            -- Total Peminjaman keseluruhan
-                            (SELECT COUNT(*) FROM PEMINJAMAN) AS total_peminjaman,
-                            
-                            -- Total Pengembalian
-                            (SELECT COUNT(*) FROM PENGEMBALIAN) AS total_pengembalian,
-                            
-                            -- Total Denda
-                            (SELECT ISNULL(SUM(denda), 0) FROM PENGEMBALIAN) AS total_denda;";
+                    string query = "SELECT * FROM vw_TotalLaporan";
 
                     using (SqlCommand cmd = new SqlCommand(query, conn))
+                    using (SqlDataReader reader = cmd.ExecuteReader())
                     {
-                        using (SqlDataReader reader = cmd.ExecuteReader())
+                        if (reader.Read())
                         {
-                            if (reader.Read())
-                            {
-                                lblPeriode.Text = reader["periode"].ToString();
-                                lblTotalKunjungan.Text = Convert.ToInt32(reader["total_kunjungan"]).ToString("N0") + " orang";
-                                lblPeminjaman.Text = Convert.ToInt32(reader["total_peminjaman"]).ToString("N0") + " buku";
-                                lblPengembalian.Text = Convert.ToInt32(reader["total_pengembalian"]).ToString("N0") + " buku";
-                                lblDenda.Text = "Rp " + Convert.ToDecimal(reader["total_denda"]).ToString("N0");
-                            }
-                            else
-                            {
-                                ResetLabels();
-                            }
+                            lblPeriode.Text = reader["periode_tampil"].ToString();
+                            lblTotalKunjungan.Text =
+                                Convert.ToInt32(reader["total_kunjungan"]).ToString("N0") + " orang";
+
+                            lblPeminjaman.Text =
+                                Convert.ToInt32(reader["total_peminjaman"]).ToString("N0") + " buku";
+
+                            lblPengembalian.Text =
+                                Convert.ToInt32(reader["total_pengembalian"]).ToString("N0") + " buku";
+
+                            lblDenda.Text =
+                                "Rp " + Convert.ToDecimal(reader["total_denda"]).ToString("N0");
+                        }
+                        else
+                        {
+                            ResetLabels();
                         }
                     }
                 }
             }
             catch (Exception ex)
             {
-                MessageBox.Show("Terjadi kesalahan saat memuat data:\n" + ex.Message,
-                    "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                MessageBox.Show("Terjadi kesalahan:\n" + ex.Message);
                 ResetLabels();
             }
         }
